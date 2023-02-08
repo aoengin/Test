a3f0b4c510bb6a7852bc584764e0b697bc7179c8
Ahmet Oguz Engin <85455415+AhmetOguzEngin@users.noreply.github.com>
2023-02-08 22:07:31 +0300

    a

--- app/src/main/java/test/App.java
+++ app/src/main/java/test/App.java
@@ -9,6 +9,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.printl(new App().getGreeting());
+        System.out.println(new App().getGreeting());
     }
 }
